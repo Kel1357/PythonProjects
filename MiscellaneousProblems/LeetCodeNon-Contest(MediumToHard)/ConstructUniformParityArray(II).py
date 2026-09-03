aadff7b9388443815ff5be796a31ff9413b66eff
Utkarsh <kel@DESKTOP-UEVB0MA.localdomain>
2026-09-03 06:29:25 +0000

    Added ConstructUniformParityArray(II).py in LeetCodeNon-Contest(MediumToHard) under MiscellaneousProblems

---
+++ MiscellaneousProblems/LeetCodeNon-Contest(MediumToHard)/ConstructUniformParityArray(II).py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        n=min(nums1)
+        if n%2==1:
+            return True
+        for x in nums1:
+            if x%2==1:
+                return False
+        return True
+if __name__=='__main__':
+    sol=Solution()
+    nums1=list(map(int, input("Enter Numbers (Separated By Commas):").split(',')))
+    res=sol.uniformArray(nums1)
+    print(res)
